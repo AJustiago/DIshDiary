58a8c0e56ac6a24a0e06d55e1b6a8f8bdfd8b6bd
AJustiago <alexcode0702@gmail.com>
2024-06-03 19:19:38 +0700

    fix ui

--- components/header/header.php
+++ components/header/header.php
@@ -4,9 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dish Diary</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    
     <?php if(basename($_SERVER['PHP_SELF']) == 'home.php'){ ?>
         <link href="../../pages/home/home.css" rel="stylesheet" media="all"/>
     <?php } 
@@ -26,6 +25,7 @@
     ?>
         <link href="../../pages/landingPage/landingPage.css" rel="stylesheet" media="all"/>
     <?php } ?>
+    
 </head>
 <body>
     <header class="border-bottom mb-4">
@@ -55,10 +55,5 @@
                 </div>
             </div>
         <?php endif; ?>
-    <?php
-        // Echo or print the current page name
-        echo "Current Page: $current_page";
-        echo "Style: $css_file";
-    ?>
     </header>
     <main>
