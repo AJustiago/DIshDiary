5a62502ec8d586368b8b22a016de9e7e14b87286
AJustiago <alexcode0702@gmail.com>
2024-06-03 10:30:22 +0700

    up header 1.0.0

--- components/header/header.php
+++ components/header/header.php
@@ -3,18 +3,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Website</title>
-    <link rel="stylesheet" href="styles.css">
+    <title>Dish Diary</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../components/header/header.css">
 </head>
 <body>
-    <header>
-        <h1>Welcome to My Website</h1>
-        <nav>
-            <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="about.php">About</a></li>
-                <li><a href="contact.php">Contact</a></li>
-            </ul>
-        </nav>
+    <header class="border-bottom mb-4">
+    <?php
+        $current_page = basename($_SERVER['PHP_SELF']);
+        if ($current_page == 'landingPage.php'): ?>
+            <div class="row py-2 align-items-center justify-content-between mx-5 px-0">
+                <div class="nav-left d-flex align-items-center">
+                    <a href="" class="nav-link">About Us</a>
+                    <a href="" class="nav-link">F&Q</a>
+                </div>
+                <div class="nav-right d-flex align-items-center">
+                    <a href="" class="nav-link">Login</a>
+                </div>
+            </div>
+        <?php else: ?>
+            <div class="row py-2 align-items-center justify-content-between mx-5 px-0">
+                <div class="nav-left d-flex align-items-center">
+                    <a href="../../pages/home/home.php" class="nav-link <?= ($current_page == 'home.php') ? 'active' : '' ?>">Home</a>
+                    <a href="../../pages/recipes/recipes.php" class="nav-link <?= ($current_page == 'recipes.php') ? 'active' : '' ?>">My Recipe</a>
+                    <a href="../../pages/favorites/favorites.php" class="nav-link <?= ($current_page == 'favorites.php') ? 'active' : '' ?>">Favorite</a>
+                </div>
+                <div class="title text-center font-weight-bold">Dish Diary</div>
+                <div class="nav-right d-flex align-items-center">
+                    <a href="../../pages/account/account.php" class="nav-link <?= ($current_page == 'account.php') ? 'active' : '' ?>">Account</a>
+                    <div class="profile-logo ml-2"></div>
+                </div>
+            </div>
+        <?php endif; ?>
     </header>
     <main>
---
+++ pages/landingPage/landingPage.php
@@ -0,0 +1,5 @@
+<?php include '../../components/header/header.php'; ?>
+
+<p>This is the Landing page content.</p>
+
+<?php include '../../components/footer/footer.php'; ?>
